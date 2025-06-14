333f4c4f63cd6303e69773de27dfe24d4a61fee1
DirusLupito <dirus.lupito@gmail.com>
2025-06-13 22:53:31 -0400

    Fixed EMC duplication bug from stacking

--- EquivalentExchange/Common/GlobalItems/EMCGlobalItem.cs
+++ EquivalentExchange/Common/GlobalItems/EMCGlobalItem.cs
@@ -26,17 +26,26 @@ namespace EquivalentExchange.Common.GlobalItems
 
         // Set default EMC values based on item properties
         public override void SetDefaults(Item item) {
-            // Example: Base EMC on sell value as a starting point
+            // Base EMC on sell value as a starting point
             emc = item.value;
-            // etc.
+            // Todo: Base EMC on crafting components and fundamental item EMC values
         }
 
         // Add EMC value to tooltips
-        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
-            // Add EMC tooltip after other tooltips - color formatting can be adjusted
-            tooltips.Add(new TooltipLine(Mod, "EMCValue", $"EMC: {emc}") { 
-                OverrideColor = new Color(255, 255, 255) 
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            // Add EMC tooltip after other tooltips
+            tooltips.Add(new TooltipLine(Mod, "EMCValue", $"EMC: {emc}")
+            {
+                OverrideColor = new Color(255, 255, 255)
             });
+            // Add stack EMC value if applicable
+            if (item.stack > 1) {
+                tooltips.Add(new TooltipLine(Mod, "StackEMCValue", $"Stack EMC: {emc * item.stack}")
+                {
+                    OverrideColor = new Color(200, 200, 200)
+                });
+            }
         }
 
         // Save EMC value when the game is saved
@@ -47,18 +56,6 @@ namespace EquivalentExchange.Common.GlobalItems
         // Load EMC value when the game is loaded
         public override void LoadData(Item item, TagCompound tag) {
             emc = tag.GetLong("EMC");
-        }
-
-        // Handle stacking of items with EMC
-        public override void OnStack(Item destination, Item source, int numTransferred) {
-            // When items stack, make sure EMC transfers properly
-            EMCGlobalItem sourceEMC = source.GetGlobalItem<EMCGlobalItem>();
-            
-            // Calculate proportional EMC from the source stack
-            if (source.stack > 0) {
-                long emcPerItem = sourceEMC.emc / source.stack;
-                emc += emcPerItem * numTransferred;
-            }
         }
     }
 }
